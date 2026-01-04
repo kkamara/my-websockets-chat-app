bc87da83c30f73cb32a328de537b64da50739dbb
kkamara <kamaracomputers@gmail.com>
2026-01-04 12:27:22 +0000

    Live update messages when new message submitted

--- frontend/src/components/pages/chat/MessageBoxComponent.jsx
+++ frontend/src/components/pages/chat/MessageBoxComponent.jsx
@@ -5,8 +5,11 @@ import { useSelector, useDispatch, } from 'react-redux'
 import { setOpenChat } from '../../../redux/actions/openChatActions'
 import { getMessages } from '../../../redux/actions/messagesActions'
 import { createMessageAction } from '../../../redux/actions/createMessageActions'
+import io from "socket.io-client"
 
 import "./MessageBoxComponent.scss"
+
+let socket
 
 const MessageBoxComponent = () => {
   const state = useSelector(state => ({
@@ -19,11 +22,19 @@ const MessageBoxComponent = () => {
 
   const [message, setMessage] = useState("")
   const [messages, setMessages] = useState([])
+  const [socketConnected, setSocketConnected] = useState(false)
+  const [typing, setTyping] = useState(false)
+  const [isTyping, setIsTyping] = useState(false)
 
   useEffect(() => {
     if (false === state.openChat.loading && state.openChat.data) {
       setOpen(true)
       dispatch(getMessages(state.openChat.data.id))
+      
+      socket.emit("join chat", {
+        userID: JSON.parse(localStorage.getItem("user")).id,
+        chatID: state.openChat.data.id,
+      })
     }
   }, [state.openChat])
 
@@ -36,8 +47,45 @@ const MessageBoxComponent = () => {
   useEffect(() => {
     if (false === state.createMessage.loading && state.createMessage.data) {
       setMessages([...messages, state.createMessage.data])
+      socket.emit("new message", {
+        ...state.createMessage.data,
+        to: state.openChat.data.to,
+      })
     }
   }, [state.createMessage])
+
+  useEffect(() => {
+    socket = io("http://localhost:8000")
+    socket.emit("setup", JSON.parse(localStorage.getItem("user")))
+    socket.on("connected", () => setSocketConnected(true))
+    socket.on("typing", () => setIsTyping(true))
+    socket.on("stop typing", () => setIsTyping(false))
+
+    return () => {
+      socket.off("connected")
+      socket.off("typing")
+      socket.off("stop typing")
+    }
+  }, [])
+
+  useEffect(() => {
+    socket.on("message received", newMessageReceived => {
+      // if (
+      //   !selectedChatCompare ||
+      //   selectedChatCompare._id !== newMessageReceived.chat._id
+      // ) {
+      //   if (!notification.includes(newMessageReceived)) {
+      //     setNotification([newMessageReceived, ...notification]);
+      //     setFetchAgain(!fetchAgain);
+      //   }
+      // } else {
+        setMessages([...messages, newMessageReceived]);
+      // }
+    });
+    return () => {
+      socket.off("message received");
+    };
+  })
 
   const handleCloseMessageBox = () => {
     dispatch(setOpenChat(null))

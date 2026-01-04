b74b58dac06e9951ac07b0a2758a64211089263c
kkamara <kamaracomputers@gmail.com>
2026-01-04 01:32:39 +0000

    This chat is designed for larger screens.

--- frontend/src/components/pages/chat/ChatComponent.jsx
+++ frontend/src/components/pages/chat/ChatComponent.jsx
@@ -3,6 +3,18 @@ import ChatSidebar from './ChatSidebar';
 import MessageBoxComponent from './MessageBoxComponent';
 
 const ChatComponent = () => {
+  if (992 > window.innerWidth) {
+    return (
+      <div className="chat-component-container container">
+        <div className="row">
+          <div className="col-12 text-center">
+            This chat is designed for larger screens. Please use a desktop device.
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="chat-component-container container">
       <div className="row">
